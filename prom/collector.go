ca39237de0c2d70921de0cf37425cc937b21ca64
Naoya Otsuka <otknoy@gmail.com>
2022-07-05 20:44:44 +0900

    implement prometheus collector

---
+++ prom/collector.go
@@ -0,0 +1,57 @@
+package prom
+
+import (
+	"context"
+	"log"
+	"roomctl/switchbot"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+const (
+	namespace = "roomctl"
+)
+
+var (
+	temperatureGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: namespace,
+		Name:      "temperature",
+		Help:      "temperature",
+	})
+	humidityGauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: namespace,
+		Name:      "humidity",
+		Help:      "humidity",
+	})
+)
+
+var _ prometheus.Collector = (*SwitchBotSensorCollector)(nil)
+
+type SwitchBotSensorCollector struct {
+	Client switchbot.Client
+}
+
+func (c *SwitchBotSensorCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- temperatureGauge.Desc()
+	ch <- humidityGauge.Desc()
+}
+
+func (c *SwitchBotSensorCollector) Collect(ch chan<- prometheus.Metric) {
+	ctx := context.Background()
+
+	deviceId := "ED1937346E87"
+
+	temp, hum, err := c.Client.GetMetrics(ctx, deviceId)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println(temp, hum)
+
+	temperatureGauge.Set(float64(temp))
+	humidityGauge.Set(float64(hum))
+
+	ch <- temperatureGauge
+	ch <- humidityGauge
+}
