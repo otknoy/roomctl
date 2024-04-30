73730ef59e17bf99ea9d2cdeefb9d2bdf456ba04
Naoya Otsuka <otknoy@gmail.com>
2024-04-30 11:30:39 +0900

    temp

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os/signal"
 	"roomctl/collector"
 	"roomctl/config"
+	"syscall"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	c, err := config.Load()
 	if err != nil {
 		log.Fatalln(err)
@@ -27,7 +33,19 @@ func main() {
 		prometheus.MustRegister(col)
 	}
 
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", c.Port), nil); err != http.ErrServerClosed {
-		log.Fatal(err)
+	done := make(chan error, 1)
+	go func() {
+		done <- http.ListenAndServe(fmt.Sprintf(":%d", c.Port), nil)
+	}()
+
+	select {
+	case err := <-done:
+		log.Println(err)
+	case <-ctx.Done():
+		log.Println("done")
 	}
+
+	// if err := http.ListenAndServe(fmt.Sprintf(":%d", c.Port), nil); err != http.ErrServerClosed {
+	// 	log.Fatal(err)
+	// }
 }
